d151fc92123b39c185f620c7023cb6f002bf055f
anjessen <antoinejessen19@gmail.com>
2021-03-04 18:13:06 +0100

    boostrap

--- models/model.js
+++ models/model.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose");
 
-const TodoSchema = new mongoose.Schema({
+const TodoSchema = mongoose.Schema({
   todo: {
     type: String,
     required: true,
   },
 });
 
-module.exports = new mongoose.model("Todo", TodoSchema);
+module.exports = mongoose.model("Todo", TodoSchema);
